6731c44720ac4e76431ad02592b17baf9b3de918
Benedict Thompson <benjfthompson@gmail.com>
2019-10-15 13:53:10 +0100

    Renamed tests class
    Signed-off-by: Benedict Thompson <benjfthompson@gmail.com>

--- automod/chatbot/chatbot_tests.py
+++ automod/chatbot/chatbot_tests.py
@@ -3,7 +3,7 @@ import unittest
 from .chatbot import ChatBot
 
 
-class MyTestCase(unittest.TestCase):
+class ChatBotTests(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         self.chat_bot = ChatBot()
