9673f849ecbcf39e70d59f0f1aa09bdbfc55d14f
Herman Lee <leeh91@uni.coventry.ac.uk>
2019-10-23 11:11:10 +0100

    implemented discord bot

--- automod/chat/chat_discord.py
+++ automod/chat/chat_discord.py
@@ -1,15 +1,28 @@
 from . import ChatBase
+import discord
 
 
 class ChatDiscord(ChatBase):
-    def on_message_received(self, user_id, message):
-        pass  # TODO
+    def __init__(self, chat_bot, token):
+        super().__init__(chat_bot)
+        self.bot = token
+        self.client = discord.Client()
 
-    def broadcast_message(self, message) -> None:
-        pass  # TODO
+    def find_user_id(self, user_name):
+        for user in self.client.get_all_members():
+            if user.name == user_name:
+                return user.id
+        return None
 
-    def send_message_to(self, user_id, message) -> None:
-        pass  # TODO
+    async def broadcast_message(self, message, channel_id) -> None:
+        ch = self.client.get_channel(channel_id)
+        await ch.send(message)
+        await self.client.close()
+
+    async def send_message_to(self, user_name, message) -> None:
+        user_id = self.find_user_id(user_name)
+        user = self.client.get_user(user_id)
+        await user.send(message)
 
     def send_ban_req(self, user_id, reason=None) -> bool:
         pass  # TODO
--- automod/chat/chat_discord.pyi
+++ automod/chat/chat_discord.pyi
@@ -1,9 +1,17 @@
+from automod.chatbot import ChatBot
 from . import ChatBase
+from discord import Client
 
 
 class ChatDiscord(ChatBase):
-    def on_message_received(self, user_id: str, message: str) -> None:
-        ...
+
+    def __init__(self, chat_bot: ChatBot, token: str) -> None:
+        super().__init__(chat_bot)
+        self.chat_bot: ChatBot = chat_bot
+        self.token: str = token
+        self.client: Client = Client()
+
+    def find_user_id(self, user_name: str) -> int:
 
     def broadcast_message(self, message: str) -> None:
         ...
