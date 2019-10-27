ea1fba6ee97ac41808c2bf205ac376ae0da831ea
Prinex <38051195+Prinex@users.noreply.github.com>
2019-10-27 04:30:13 +0200

    Created a new branch for twitch API layer and added the necessary files

---
+++ automod/chat/chat_base.pyi
@@ -0,0 +1,24 @@
+import abc
+
+from automod.chatbot import ChatBot
+
+
+class ChatBase(abc.ABC):
+    def __init__(self, chat_bot: ChatBot) -> None:
+        ...
+
+    @abc.abstractmethod
+    def broadcast_message(self, ch_name:str, message: str) -> None:
+        ...
+
+    @abc.abstractmethod
+    def send_message_to(self, user_id: str, message: str) -> None:
+        ...
+
+    @abc.abstractmethod
+    def send_ban_req(self, user_id: str, reason: str = None) -> bool:
+        ...
+
+    @abc.abstractmethod
+    def send_mute_req(self, user_id: str, reason: str = None) -> bool:
+        ...
